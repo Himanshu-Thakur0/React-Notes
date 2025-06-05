f18b56d3894a2208db7ea47bdbdc8617aa5eaa6e
Himanshu Thakur <h.thakur.work@gmail.com>
2025-06-06 02:13:50 +0530

    add redux toolkit todo

--- Redux Toolkit/redux-todo/src/App.tsx
+++ Redux Toolkit/redux-todo/src/App.tsx
@@ -1,11 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <>
-      
+      <h1>Redux ToolKit Todo App</h1>
+      <AddTodo/>
+      <Todo/>
     </>
   )
 }
